1a90c4d955275dd16001bd5de2197a44af331e4d
Balint Solyom <solyom.balint@gmail.com>
2020-11-19 14:10:15 +0100

    humpty and helloothers done

---
+++ week-01/day-3/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Győző!");
+        System.out.println("Hello Géza!");
+        System.out.println("Hello Zakariás!");
+    }
+}
