ae633860925cb2e4882059250cca5035aec87d78
Balint Solyom <solyom.balint@gmail.com>
2020-11-19 14:39:05 +0100

    added classes introducing variable types

---
+++ week-01/day-3/src/NumberOperations.java
@@ -0,0 +1,14 @@
+public class NumberOperations {
+    public static void main(String[] args) {
+        // Arithmetric operations
+        System.out.println(1 + 1); // Prints 2
+        System.out.println(5 - 2); // Prints 3
+        System.out.println(3 * 4); // Prints 12
+        System.out.println(6 / 2); // Prints 3
+        System.out.println(5.0 / 2); // Prints 2
+        System.out.println(5. / 2); // Prints 2.5
+        System.out.println(5 / 2.); // Prints 2.5
+        System.out.println(5. / 2.); // Prints 2.5
+        System.out.println(5 % 2); // Prints 1
+    }
+}
---
+++ week-01/day-3/src/Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+    public static void main(String[] args) {
+        // String and special characters
+        System.out.println("apple"); // Prints apple
+        System.out.println("don't"); // Prints don't
+        System.out.println("\"Everything you can imagine is real.\" - Picasso"); // Prints "Everything you can imagine is real." - Picasso
+
+        // String Operators
+        // Concatination
+        System.out.println("tooth" + "brush"); // Prints toothbrush
+
+        // Concat string with number
+        System.out.println("My favorite number is: " + 8); // Prints My favorite number is: 8
+    }
+}
