4087c4911e7b41178c4b7f12f5d0a99b578b477f
Balint Solyom <solyom.balint@gmail.com>
2020-11-23 13:01:52 +0100

    week 2 created

--- week-01/day-4/src/DrawPyramid.java
+++ week-01/day-4/src/DrawPyramid.java
@@ -5,11 +5,11 @@ public class DrawPyramid {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("How big should be the pyramid like? (whole number)");
-        int input = scanner.nextInt();
+        int height = scanner.nextInt();
 
-        for (int i = 1; i <= input; i++) {//outer loop to print lines
-        for (int j = 1 ; j < (input + i); j++) {//length of line is input + 1
-            if(j > (input - i)) { // if pixel no is greater than input - rownum then draw star
+        for (int i = 1; i <  (height); i++) {//outer loop to print lines
+        for (int j = 1 ; j < (height + i); j++) {//length of line is input + 1
+            if(j > (height - i)) { // if pixel no is greater than input - rownum then draw star
             System.out.print("*");} else {
                 System.out.print(" ");
             }
@@ -17,3 +17,27 @@ public class DrawPyramid {
         System.out.println();}
     }
 }
+
+/*      --*
+        -***
+        *****
+        base 5 height 3
+
+        ---*
+        --***
+        -*****
+        *******
+        base 7 height 4
+
+        ----*
+        ---***
+        --*****
+        -*******
+        *********
+        base 9 height 5
+
+        row count -> i < height
+        row length -> j < height + i
+        isStar -> j > height - i*/
+
+
