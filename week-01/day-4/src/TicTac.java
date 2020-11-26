cdc949180ad251b466e48e0608352b6521bd060f
Balint Solyom <solyom.balint@gmail.com>
2020-11-26 22:33:43 +0100

    tictac game created

--- week-01/day-4/src/TicTac.java
+++ week-01/day-4/src/TicTac.java
@@ -2,94 +2,118 @@ import java.util.Scanner;
 
 public class TicTac {
 
-    public static void drawBoard (String[][] board) {
-        //draw board
+    //function to draw board
+    public static void drawBoard(String[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j]+ " ");
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
             System.out.println();
         }
     }
 
-    public static boolean checkGameEnd (String[][] board) {
+    //function to check is there is a winner
+    public static boolean checkGameEnd(String[][] board) {
+        String blank = "[ ]";
+        boolean result = false;
         //check rows
         for (int i = 0; i < board.length; i++) {
-            if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
-                return true;
+            if (board[i][0] != blank && board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2])) {
+                result = true;
             }
         }
         //check columns
         for (int i = 0; i < board.length; i++) {
-            if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-                return true;
+            if (board[0][i] != blank && board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i])) {
+                result = true;
             }
         }
         //check diagonals
-        if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
-            return true;
+        if (board[0][0] != blank && board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2])) {
+            result = true;
         }
-        return false;
-
+        return result;
     }
 
     public static void main(String[] args) {
 
+        //declare the game board
         String[][] board = {
-                {"[ ]","[ ]","[ ]"},
-                {"[ ]","[ ]","[ ]"},
-                {"[ ]","[ ]","[ ]"}
+                {"[ ]", "[ ]", "[ ]"},
+                {"[ ]", "[ ]", "[ ]"},
+                {"[ ]", "[ ]", "[ ]"}
         };
-
-        drawBoard(board);
 
         //set up scanner and variables
         Scanner scanner = new Scanner(System.in);
         int coordinateA;
         int coordinateB;
-
-
-        //get coordinates
-        //userA
-
-        String xCoordPromt = "Enter the x coordinate (1-3)";
-        String yCoordPromt = "Enter the x coordinate (1-3)";
+        String xCoordPrompt = "Enter the x coordinate (1-3)";
+        String yCoordPrompt = "Enter the y coordinate (1-3)";
         int playerOnTurn = 1;
+        boolean isDraw = false;
 
-        System.out.println("Player " + playerOnTurn + "'s turn");
-        do { //loop until the coordinates point to an empty cell
-        do {System.out.println(xCoordPromt); //loop until input is valid
-        coordinateA = scanner.nextInt();}
-            while (coordinateA < 1 || coordinateA > 3);
-            //System.out.println(userACoordinateA);
-        do {System.out.println(yCoordPromt); //loop until input is valid
-            coordinateB = scanner.nextInt();}
-        while (coordinateB < 1 || coordinateB > 3);}
-        while (board [coordinateB-1][coordinateA-1] != "[ ]");
-
-        // add input to the board
-        if (playerOnTurn == 1)
-        {board [coordinateB-1][coordinateA-1] = "[x]";}
-        else {board [coordinateB-1][coordinateA-1] = "[o]";}
-        if (playerOnTurn == 1) {playerOnTurn = 2;} else {playerOnTurn = 1};
-
-        /*
-        System.out.println("Player 2's turn");
-        do { //loop until the coordinates point to an empty cell
-        do {System.out.println(xCoordPromt);
-            userBCoordinateA = scanner.nextInt();}
-        while (userBCoordinateA < 1 || userBCoordinateA > 3);
-        //System.out.println(userACoordinateA);
-        do {System.out.println(yCoordPromt);
-            userBCoordinateB = scanner.nextInt();}
-        while (userACoordinateB < 1 || userBCoordinateB > 3);}
-        while (board [userBCoordinateB-1][userBCoordinateA-1] != "[ ]");
-
-        // add input to the board
-        board [userBCoordinateB-1][userBCoordinateA-1] = "[o]";
-*/
+        //draw board the first time
         drawBoard(board);
 
+        //main loop to play game
+        for (int i = 0; i < 9; i++) {
+
+            System.out.println("Player " + playerOnTurn + "'s turn");
+
+            //ask for coordinates. loop until the coordinates point to an empty cell
+            do {
+                //ask for x coordinate. loop until input is valid
+                do {
+                    System.out.println(xCoordPrompt);
+                    coordinateA = scanner.nextInt();
+                }
+                while (coordinateA < 1 || coordinateA > 3);
+                //ask for y coordinate. loop until input is valid
+                do {
+                    System.out.println(yCoordPrompt);
+                    coordinateB = scanner.nextInt();
+                }
+                while (coordinateB < 1 || coordinateB > 3);
+            }
+            while (board[coordinateB - 1][coordinateA - 1] != "[ ]");
+
+            // "draw" input to the board
+            if (playerOnTurn == 1) {
+                board[coordinateB - 1][coordinateA - 1] = "[x]";
+            } else {
+                board[coordinateB - 1][coordinateA - 1] = "[o]";
+            }
+
+            //draw the board to show the user what has been inputted
+            drawBoard(board);
+
+            //check if game has ended
+            if (checkGameEnd(board)) {
+                break;
+            }
+
+            //draw game indicator.
+            if (i == 8) {
+                isDraw = true;
+            }
+
+            //next player's turn
+            if (playerOnTurn == 1) {
+                playerOnTurn = 2;
+            } else {
+                playerOnTurn = 1;
+            }
         }
+
+        //announce winner
+        String winnerAnnouncement;
+        if (isDraw) {
+            winnerAnnouncement = "Draw game...";
+        } else {
+            winnerAnnouncement = "Player " + playerOnTurn + " has won the game. Congrats!";
+        }
+        System.out.println(winnerAnnouncement);
     }
+}
