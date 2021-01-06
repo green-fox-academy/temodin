ce66db27a69e7cd4f48fc573b74c3a1ab339df60
Balint Solyom <solyom.balint@gmail.com>
2021-01-06 14:06:36 +0100

    helloworld controller and rest app done

---
+++ week-06/day-3/springstart/src/main/java/com/greenfoxacademy/springstart/MainController.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.springstart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value="/")
+
+    @ResponseBody
+    public String hello () {
+        return "Hello gyoker!";
+    }
+}
