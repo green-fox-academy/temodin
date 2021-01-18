827d2bb424657b864fcd4a180ebe20bfbc5f9955
Balint Solyom <solyom.balint@gmail.com>
2021-01-18 17:54:01 +0100

    update done

--- week-08/day-1/todos_sql/src/main/java/com/example/todo_sql/demo/controller/TodoController.java
+++ week-08/day-1/todos_sql/src/main/java/com/example/todo_sql/demo/controller/TodoController.java
@@ -3,16 +3,15 @@ package com.example.todo_sql.demo.controller;
 
 import com.example.todo_sql.demo.model.Todo;
 import com.example.todo_sql.demo.repository.TodoRepository;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -45,13 +44,41 @@ public class TodoController {
     public String list(Model model, @RequestParam(required = false) Boolean done) {
         List<Todo> todos = new ArrayList<>();
 
-        if (done == null) {todos = (List<Todo>) todoRepository.findAll();}
-        else {
-            todos = ((List<Todo>)todoRepository.findAll()).stream().filter(t -> t.getDone() == done).collect(Collectors.toList());
+        if (done == null) {
+            todos = (List<Todo>) todoRepository.findAll();
+        } else {
+            todos = ((List<Todo>) todoRepository.findAll()).stream().filter(t -> t.getDone() == done).collect(Collectors.toList());
         }
         model.addAttribute("todos", todos);
         return "todolist";
     }
 
+    @PostMapping("/{id}/delete")
+    public String deleteToDo(@PathVariable long id) {
+        todoRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String getEditToDo(@PathVariable long id, Model model) {
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        Todo todo;
+        if (optionalTodo.isPresent()) {
+            todo = optionalTodo.get();
+        } else {
+            return "redirect:/";
+        }
+        model.addAttribute("id", todo.getId());
+        model.addAttribute("title", todo.getTitle());
+        model.addAttribute("urgent", todo.getUrgent());
+        model.addAttribute("done", todo.getDone());
+        return "edittodo";
+    }
+
+    @PostMapping("/{id}/edit-todo")
+    public String postEditToDo(@PathVariable long id, String title, Boolean urgent, Boolean done) {
+        todoRepository.save(new Todo(id, title, urgent, done));
+        return "redirect:/";
+    }
 
 }
--- week-08/day-1/todos_sql/src/main/java/com/example/todo_sql/demo/model/Todo.java
+++ week-08/day-1/todos_sql/src/main/java/com/example/todo_sql/demo/model/Todo.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
 
 public class Todo {
    @Id
